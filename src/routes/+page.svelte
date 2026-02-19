6ade607e1164c24e753bf2edbb8aae47c4a80ddf
adwidianjaya <adwidianjaya@gmail.com>
2026-02-19 10:57:07 +0700

    add PromptInput

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,167 +9,107 @@
   import logo from "$lib/assets/logo-bg.png";
   import LogViewer from "./LogViewer.svelte";
   import SchemaViewer from "./SchemaViewer.svelte";
+  import PromptInput from "./PromptInput.svelte";
+
+  let definition = $state(initDefinition());
+  // $inspect("...definition", definition);
+
+  let schema = $state({
+    elements: [],
+    states: {},
+  });
+  // $inspect("...schema", schema);
+
+  let logs = $state([]);
+  // $inspect("...logs", logs);
 
   let prompt = $state(
     "create form, input name, and output simple greeting. The greeting should be in the form of 'Hello, {name}!' with orange text.",
     // "change to Bahasa Indonesia",
   );
   // $inspect("...prompt", prompt);
-  let definition = $state(initDefinition());
-  // let definition = $state({
-  //   root: "login-container",
-  //   states: {
-  //     form: {
-  //       name: "",
-  //     },
-  //   },
-  //   elements: {
-  //     "login-container": {
-  //       type: "Card",
-  //       props: {
-  //         title: "Greeting Form",
-  //         description: "Enter your name to see a greeting",
-  //         maxWidth: "sm",
-  //         centered: true,
-  //       },
-  //       children: ["name-input", "greeting-text"],
-  //     },
-  //     "name-input": {
-  //       type: "ShortText",
-  //       props: {
-  //         label: "Name",
-  //         placeholder: "Enter your name",
-  //         value: "$states.form.name",
-  //       },
-  //     },
-  //     "greeting-text": {
-  //       type: "Text",
-  //       props: {
-  //         text: "{$states.form.name ? 'Hello, ' + $states.form.name + '!' : ''}",
-  //         level: "p",
-  //         class: "text-orange-500 font-bold",
-  //       },
-  //     },
-  //   },
-  // });
-  // $inspect("...definition", definition);
-  let schema = $state({
-    elements: [],
-    states: {},
-  });
-  // let schema = $state({
-  //   elements: [
-  //     {
-  //       type: "Card",
-  //       props: {
-  //         title: "Greeting Form",
-  //         description: "Enter your name to see a greeting",
-  //         maxWidth: "sm",
-  //         centered: true,
-  //       },
-  //       children: [
-  //         {
-  //           type: "ShortText",
-  //           props: {
-  //             label: "Name",
-  //             value: "$states.form.name",
-  //           },
-  //         },
-  //         {
-  //           type: "Text",
-  //           props: {
-  //             text: "{$states.form.name ? 'Hello, ' + $states.form.name + '!' : ''}",
-  //             level: "p",
-  //             class: "text-orange-500 font-bold",
-  //           },
-  //         },
-  //       ],
-  //     },
-  //   ],
-  //   states: {
-  //     form: {
-  //       name: "",
-  //     },
-  //   },
-  // });
-  // $inspect("...schema", schema);
-  let logs = $state([]);
-  // $inspect("...logs", logs);
 
-  const sendPrompt = async () => {
-    let body = JSON.stringify({
-      prompt,
-      context: {
-        ...definition,
-      },
-    });
-    prompt = "";
-    const response = await fetch("/api/chat", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body,
-    });
+  let loading = $state(false);
+  const handleSendPrompt = async () => {
+    loading = true;
 
-    const reader = response.body
-      ?.pipeThrough(new TextDecoderStream())
-      .getReader();
-    if (!reader) return;
+    try {
+      let body = JSON.stringify({
+        prompt,
+        context: {
+          ...definition,
+        },
+      });
+      // prompt = "";
+      const response = await fetch("/api/chat", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body,
+      });
 
-    let lastLine = "";
-    while (true) {
-      const { value, done } = await reader.read();
-      // console.log({ value, done });
-      if (done) {
-        break;
-      }
+      const reader = response.body
+        ?.pipeThrough(new TextDecoderStream())
+        .getReader();
+      if (!reader) return;
 
-      let lines = value.split("\n");
-      for (let index = 0; index < lines.length; index++) {
-        const line = lines[index];
-        lastLine += line;
+      let lastLine = "";
+      while (true) {
+        const { value, done } = await reader.read();
+        // console.log({ value, done });
+        if (done) {
+          break;
+        }
 
-        if (index < lines.length - 1) {
-          lastLine = String(lastLine).trim();
+        let lines = value.split("\n");
+        for (let index = 0; index < lines.length; index++) {
+          const line = lines[index];
+          lastLine += line;
 
-          const { operation } = updateDefinitionByOperationString({
-            operationString: lastLine,
-            definition,
-          });
-          if (operation) {
-            logs.push({
-              operation,
-              message: lastLine,
+          if (index < lines.length - 1) {
+            lastLine = String(lastLine).trim();
+
+            const { operation } = updateDefinitionByOperationString({
+              operationString: lastLine,
+              definition,
             });
-          }
-          schema = convertDefinitionToRenderSchema({
-            definition,
-            initialStates: schema.states,
-          });
+            if (operation) {
+              logs.push({
+                operation,
+                message: lastLine,
+              });
+            }
+            schema = convertDefinitionToRenderSchema({
+              definition,
+              initialStates: schema.states,
+            });
 
-          lastLine = "";
+            lastLine = "";
+          }
         }
       }
-    }
 
-    lastLine = String(lastLine).trim();
-    if (lastLine) {
-      const { operation } = updateDefinitionByOperationString({
-        operationString: lastLine,
-        definition,
-      });
-      if (operation) {
-        logs.push({
-          operation,
-          message: lastLine,
+      lastLine = String(lastLine).trim();
+      if (lastLine) {
+        const { operation } = updateDefinitionByOperationString({
+          operationString: lastLine,
+          definition,
         });
-      }
-      schema = convertDefinitionToRenderSchema({
-        definition,
-        initialStates: schema.states,
-      });
+        if (operation) {
+          logs.push({
+            operation,
+            message: lastLine,
+          });
+        }
+        schema = convertDefinitionToRenderSchema({
+          definition,
+          initialStates: schema.states,
+        });
 
-      lastLine = "";
-    }
+        lastLine = "";
+      }
+    } catch (err) {}
+
+    loading = false;
   };
 </script>
 
@@ -190,31 +130,7 @@
     <div class={["w-1/4 flex flex-col", "border-b border-t border-gray-700"]}>
       <SchemaViewer {schema} />
 
-      <div class="h-32 flex-none border-t border-gray-700 flex flex-col">
-        <textarea
-          placeholder="Talk to Flaz here..."
-          onkeydown={e => {
-            if (e.key === "Enter" && !e.shiftKey) {
-              e.preventDefault();
-              sendPrompt();
-              return;
-            }
-          }}
-          bind:value={prompt}
-          class="w-full h-full outline-0 text-sm px-3 py-2"></textarea>
-        <div class="text-right flex-none">
-          <button
-            type="button"
-            onclick={sendPrompt}
-            class={[
-              "px-2 pb-2 cursor-pointer",
-              "text-gray-200 hover:text-gray-400",
-              "transition duration-100",
-            ]}>
-            <CircleArrowRight class="w-6 h-6" />
-          </button>
-        </div>
-      </div>
+      <PromptInput bind:prompt {handleSendPrompt} {loading} />
     </div>
 
     <div
---
+++ src/routes/PromptInput.svelte
@@ -0,0 +1,36 @@
+<script>
+  import { CircleArrowRight } from "lucide-svelte";
+
+  let { prompt = $bindable(), handleSendPrompt, loading } = $props();
+</script>
+
+<div class="h-32 flex-none border-t border-gray-700 flex flex-col">
+  <textarea
+    placeholder="Talk to Flaz here..."
+    onkeydown={e => {
+      if (e.key === "Enter" && !e.shiftKey) {
+        e.preventDefault();
+        handleSendPrompt?.();
+        return;
+      }
+    }}
+    bind:value={prompt}
+    class={[
+      "w-full h-full outline-0 text-sm px-3 py-2",
+      loading
+        ? "animate-pulse pointer-events-none text-gray-400"
+        : "text-white",
+    ]}></textarea>
+  <div class="text-right flex-none">
+    <button
+      type="button"
+      onclick={handleSendPrompt}
+      class={[
+        "px-2 pb-2 cursor-pointer",
+        "text-gray-200 hover:text-gray-400",
+        "transition duration-100",
+      ]}>
+      <CircleArrowRight class="w-6 h-6" />
+    </button>
+  </div>
+</div>
